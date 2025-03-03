dd5b7d9b9619b145be2c2b5c5c5047c154ab8f6f
Nicolas  Muñoz <44203419+Nicolasmp92@users.noreply.github.com>
2025-03-03 18:58:22 +0000

    estructurando las carpetas para el curso

---
+++ Curso JS/js/01-hola_mundo.js
@@ -0,0 +1,5 @@
+// alert ("Hola Mundo, desde un fichero externo");
+// alert ("bienvenido al curso");
+// document.write("hola desde un archivo externo");
+// console.log("muestra esto en la consola");
+// console.log(88+4);
